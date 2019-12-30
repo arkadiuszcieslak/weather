e5f7967f03e530801de14583d4ff32dc37e585fc
Arkadiusz Cieslak <arekc70@gmail.com>
2019-12-30 12:32:21 +0100

    Added weather_reducer.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
@@ -12,6 +15,9 @@ export default class SearchBar extends Component {
 
   onFormSubmit = (event) => {
     event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: ''});
   }
 
   render() {
@@ -29,3 +35,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators( { fetchWeather }, dispatch)
+};
+
+export default connect(null, mapDispatchToProps)(SearchBar);
